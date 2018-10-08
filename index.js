22fade135ad835d280df3716463f5604ee54e255
Juan Nicolas <jnma91@gmail.com>
2018-10-08 00:52:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,15 @@ function nestedTarget(){
 }
 
 function increaseRankBy(n){
-
+  var rankedLists = document.querySelectorAll('.ranked-list')
+  var currentList
+  for (var i = 0; i < rankedLists.length; i++){
+    currentList = rankedLists[i]
+    for (var j = 0; j < currentList.children.length; j++){
+      let currentNum = currentList.children[j]
+      currentNum.innerHTML += parseInt(n)
+    }
+  }
 }
 
 function deepestChild(){
